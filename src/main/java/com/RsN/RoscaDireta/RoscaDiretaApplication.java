a1390716a349c6c2f39ca982a580fb6eb8bd8c08
Rafael Marçon <raffabrasill1@gmail.com>
2024-09-24 19:52:18 -0300

    first commit

---
+++ src/main/java/com/RsN/RoscaDireta/RoscaDiretaApplication.java
@@ -0,0 +1,13 @@
+package com.RsN.RoscaDireta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoscaDiretaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoscaDiretaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/RsN/RoscaDireta/RoscaDiretaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RsN.RoscaDireta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoscaDiretaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
